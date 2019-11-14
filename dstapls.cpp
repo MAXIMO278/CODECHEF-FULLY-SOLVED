91d725e4c0dfdac299e3e1dc1d119d0ab0dbc5ab
MAXIMO278 <48401144+MAXIMO278@users.noreply.github.com>
2019-11-14 08:57:53 +0530

    Create dstapls.cpp

---
+++ dstapls.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long t;
+	cin>>t;
+	while(t--)
+		{
+			long long i,j,k,n,m=0,s1=0,s2=0,s=0,p,q;
+			cin>>n>>k;
+			s1=n/k;
+			if(s1%k==0)
+			{
+				cout<<"NO";
+			}
+			else
+			{cout<<"YES";
+			}
+			cout<<endl;
+			
+			
+			
+			
+			
+		}
+	
+	
+	
+}
